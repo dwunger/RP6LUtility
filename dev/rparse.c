e7fe4a0b2aa574c54cf23e0271fbd2a87365d28a
dwunger <106967706+dwunger@users.noreply.github.com>
2023-11-15 19:37:03 -0500

    read args vector

---
+++ dev/rparse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+int main(int argc, char *argv[]) {
+    char path[MAX_PATH];
+    char debug_path[MAX_PATH] = "X:\\SteamLibrary\\steamapps\\common\\Dying Light 2\\ph\\work\\data_platform\\pc\\assets\\common_textures_0_pc.rpack";
+    /* Check for command line arguments */
+    /*  Set default path for debugging  */
+    if (argc > 1) {
+        strcpy(path, argv[1]);
+    } else {
+        strcpy(path, debug_path);
+    }
+    printf(path);
+    /* Loop over command line arguments*/
+    for (int argument = 0; argument < argc; argument++) {
+        //printf("%s\n", argv[argument]);
+        //Do something...
+    }
+    
+
+
+
+    
+    return 0;
+}
+
