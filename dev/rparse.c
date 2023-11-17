23408787ac2ae46df56efec0e42be3cf59ea4c62
dwunger <106967706+dwunger@users.noreply.github.com>
2023-11-16 20:15:51 -0500

    file size stuff

--- dev/rparse.c
+++ dev/rparse.c
@@ -7,10 +7,26 @@
 #include <string.h>
 #include <windows.h>
 
+long int get_file_size(FILE *file) {
+    // Get current position in file
+    long int position = ftell(file);
+    if (position == -1) {
+        perror("Invalid position");
+        return -1;
+    }
+    // Jump to EOF
+    fseek(file, 0L, SEEK_END);
+    long int file_size = ftell(file);
+    
+    fseek(file, position, SEEK_SET);
+
+    return file_size;
+}
+
 int main(int argc, char *argv[]) {
     char path[MAX_PATH];
 
-    char debug_path[MAX_PATH] = DEBUG_RPACK_PATH1;
+    char debug_path[MAX_PATH] = DEBUG_RPACK_PATH2;
     //char debug_path[MAX_PATH] = ;
     /* Check for command line arguments */
     /*  Set default path for debugging  */
@@ -26,7 +42,15 @@ int main(int argc, char *argv[]) {
         //Do something...
     }
     
+    FILE *file = fopen(path, "r");
 
+    if (file == NULL) {
+        perror("Unable to access file");
+    }
+    
+    
+
+    
     return 0;
 }
 
