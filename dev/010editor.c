e9fcd1588700bbf2fba058b5830e504221e0632f
dwunger <106967706+dwunger@users.noreply.github.com>
2024-01-14 21:30:03 -0500

    header file for custom 010 editor implementations

---
+++ dev/010editor.c
@@ -0,0 +1,8 @@
+#include "010editor.h"
+#include <stdio.h>
+
+void init_010_editor() {
+    printf("Successfully initialized Simulated 010 Editor");
+}
+
+
---
+++ dev/010editor.h
@@ -0,0 +1,7 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+void init_010_editor();
+
+#endif // EDITOR_H
+
