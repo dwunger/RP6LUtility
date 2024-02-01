efe5fdff9eafb1115cf2595a42116a744cd98ce2
dwunger <106967706+dwunger@users.noreply.github.com>
2024-02-01 04:08:14 -0500

    stuff

--- dev/010editor.c
+++ dev/010editor.c
@@ -1,8 +1,54 @@
 #include "010editor.h"
 #include <stdio.h>
 
-void init_010_editor() {
-    printf("Successfully initialized Simulated 010 Editor");
+void init_emulator() {
+    printf("Emulator Initialized");
 }
+
+/**
+ * 010 Editor Manual
+ *
+ * Function: FileOpen
+ * Opens a file specified by the UTF-8 encoded string `filename`
+ * into the editor.
+ *
+ * Parameters:
+ *   - `filename` (const char): The name of the file to be opened.
+ *   - `runTemplate` (int, optional): If true, executes a template
+ *     associated with the file.
+ *   - `editAs` (char[], optional): Specifies the "Edit As" mode for
+ *     the opened file (e.g., "Hex", "Text", "Unicode").
+ *   - `openDuplicate` (int, optional): If true, creates a duplicate
+ *     copy of the file if it's already open; if false, no action is
+ *     taken.
+ *
+ * Returns:
+ *   - (int): The file index of the opened file if successful.
+ *            - The file index of the already opened file if
+ *              `openDuplicate` is false and the file is already open.
+ *            - A negative number if the file could not be opened.
+ *              No error message is displayed if the file is not found.
+ *
+ * Notes:
+ * - Can be called in a Script or in a Template with special
+ *   permission to read other files.
+ * - To open a logical drive, use `FileOpen("Drive X:")`, where X is
+ *   the drive letter.
+ * - For physical drives, use `FileOpen("Physical Drive n:")`, where
+ *   n is the drive number.
+ * - Processes can be opened using `OpenProcessById` or
+ *   `OpenProcessByName` functions.
+ */
+
+int FileOpen(const char *filename, const int runTemplate, \
+        const char *editAs, const int openDuplicate) {
+    if (filename == NULL) { 
+        perror("FileOpen: unable to open file");
+        return -1;
+    }
+    
+
+}
+
 
 
---
+++ dev/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "010editor.h"
+
+int main (void) {
+    init_emulator();
+    
+    
+    return 0;
+}
+
