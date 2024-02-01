393d2f093a06152d4e63aeec91d603847f1d43af
Denton Unger <106967706+dwunger@users.noreply.github.com>
2024-02-01 05:05:48 -0500

    Update 010editor.h
    multiline decl

--- dev/010editor.h
+++ dev/010editor.h
@@ -1,7 +1,13 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
-void init_010_editor();
+void init_emulator();
+int FileOpen(
+    const char *filename,
+    int runTemplate,
+    const char *editAs, 
+    int openDuplicate);
 
 #endif // EDITOR_H
+
 
